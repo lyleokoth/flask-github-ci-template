216f7e4e52c13d56964013b5fa4601af62de1a82
lyleokoth <okothlyleochieng@gmail.com>
2022-03-06 10:17:28 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from crypt import methods
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return {'Welcome': True}
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test_app.py
@@ -0,0 +1,13 @@
+import app 
+import unittest
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_index(self):
+        response = self.app.get('/')
+        assert response.status_code == 200
+
