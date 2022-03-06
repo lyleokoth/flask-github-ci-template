0d74c96c2365f2ec83ab4a74743efd1fc667f72e
lyleokoth <okothlyleochieng@gmail.com>
2022-03-06 10:30:14 +0300

    Corrected linting errors

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from crypt import methods
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 def index():
     return {'Welcome': True}
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,11 +1,14 @@
 import app 
 import unittest
 
+
 class MyTestCase(unittest.TestCase):
+    
 
     def setUp(self) -> None:
         app.app.testing = True
         self.app = app.app.test_client()
+
 
     def test_index(self):
         response = self.app.get('/')
