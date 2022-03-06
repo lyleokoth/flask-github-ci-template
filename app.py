145b28602db2dd48acf07942083b1e2da01fb6a9
lyleokoth <okothlyleochieng@gmail.com>
2022-03-06 11:56:07 +0300

    This will deploy to heroku

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return {'Welcome': 'Failed deploy'}
+    return {'Welcome': 'Successful deploy'}
 
 
 if __name__ == '__main__':
